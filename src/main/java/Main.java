fbdffcfe1b606c8f55a2aa264267e199c95bf1d7
陈龙 <chenlong01@souche.com>
2020-07-22 19:08:27 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import server.JerryServer;
+
+/**
+ * @author 陈龙
+ * @version 1.0
+ * @date 2020-07-22 15:27
+ * @desc 程序入口
+ */
+public class Main {
+    public static void main(String[] args) {
+        //启动服务
+        int port = 8088;
+        new JerryServer().bind(port);
+    }
+}
---
+++ src/main/java/server/JerryServer.java
@@ -0,0 +1,79 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * @author 陈龙
+ * @version 1.0
+ * @date 2020-07-22 15:45
+ */
+public class JerryServer {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public void bind(int port) {
+        //事件驱动
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        //
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap
+                    .group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            //绑定
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+            //等待服务关闭
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        protected void initChannel(SocketChannel socketChannel) {
+            socketChannel.pipeline().addLast(new JerryChannelHandlerAdapter());
+        }
+    }
+
+    private class JerryChannelHandlerAdapter extends ChannelHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//            super.channelRead(ctx, msg);
+            ByteBuf byteBuf = (ByteBuf) msg;
+            byte[] bytes = new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(bytes);
+            String body = new String(bytes, "UTF-8");
+            logger.info(body);
+            String currentTime = "query".equals(body) ? new Date().toString() : "BAD REQUEST!";
+            ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+            ctx.write(resp);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            ctx.close();
+        }
+    }
+}
